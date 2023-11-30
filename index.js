200ff3ca497b5778907da375f1a1ff2fe617cdc5
aloan93 <a.loan93@gmail.com>
2023-11-30 19:59:00 +0000

    get endpoint created for finding user by id - endpoints json updated - errors handled for user that doesnt exist

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ app.use(cors());
 app.use("/api", apiRouter);
 
 app.use((err, req, res, next) => {
+  if (err.status) {
+    res.status(err.status).send({ message: err.message });
+    return;
+  }
+  next(err);
+});
+
+app.use((err, req, res, next) => {
   console.log(err);
   res.status(500).send("Internal Server Error");
 });
--- models/users.models.js
+++ models/users.models.js
@@ -5,3 +5,14 @@ exports.fetchUsers = () => {
     return { total: result[0].length, users: result[0] };
   });
 };
+
+exports.fetchUserByUserId = (user_id) => {
+  return database
+    .query(`SELECT * FROM users WHERE user_id=?;`, [user_id])
+    .then((result) => {
+      if (result[0].length === 0) {
+        return Promise.reject({ status: 404, message: "User not found" });
+      }
+      return result[0][0];
+    });
+};
