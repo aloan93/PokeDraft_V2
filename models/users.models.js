21c4bc21dd5904bf67e0ed94f39ab5521d28139f
aloan93 <a.loan93@gmail.com>
2023-12-03 20:32:26 +0000

    route and controller made for update user by id - start made to model - helper function created in user model to check if user exists when searching on id

--- models/users.models.js
+++ models/users.models.js
@@ -1,6 +1,16 @@
 const database = require("../database/connection");
 const bcrypt = require("bcrypt");
 
+exports.checkUserExists = (user_id) => {
+  return database
+    .query(`SELECT * FROM users WHERE user_id = ?;`, [user_id])
+    .then((result) => {
+      if (result[0].length === 0) {
+        return Promise.reject({ status: 404, message: "User not found" });
+      }
+    });
+};
+
 exports.fetchUsers = () => {
   return database.query(`SELECT * FROM users`).then((result) => {
     return { total: result[0].length, users: result[0] };
@@ -8,14 +18,13 @@ exports.fetchUsers = () => {
 };
 
 exports.fetchUserByUserId = (user_id) => {
-  return database
-    .query(`SELECT * FROM users WHERE user_id=?;`, [user_id])
-    .then((result) => {
-      if (result[0].length === 0) {
-        return Promise.reject({ status: 404, message: "User not found" });
-      }
-      return result[0][0];
-    });
+  const doesUserExist = this.checkUserExists(user_id);
+  const query = database.query(`SELECT * FROM users WHERE user_id=?;`, [
+    user_id,
+  ]);
+  return Promise.all([query, doesUserExist]).then((results) => {
+    return results[0][0][0];
+  });
 };
 
 exports.createUser = (username, email, password) => {
@@ -51,3 +60,11 @@ exports.createUserLogin = (username, password) => {
       else return { status: 200, message: "Successfully logged in" };
     });
 };
+
+exports.updateUserByUserId = (
+  user_id,
+  username,
+  email,
+  password,
+  avatar_url
+) => {};
