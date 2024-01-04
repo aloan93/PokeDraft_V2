38075e80f802b7af1d446fb1bcfb645467679ca2
aloan93 <a.loan93@gmail.com>
2024-01-04 18:55:15 +0000

    added regex verification to email in create user endpoint

--- models/users.models.js
+++ models/users.models.js
@@ -62,6 +62,17 @@ exports.fetchUserByUserId = (user_id) => {
 };
 
 exports.createUser = (username, email, password) => {
+  if (
+    !/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(
+      email
+    )
+  ) {
+    return Promise.reject({
+      status: 400,
+      message: "Please provide a valid email address",
+    });
+  }
+
   return bcrypt
     .hash(password, 10)
     .then((hashedPassword) => {
